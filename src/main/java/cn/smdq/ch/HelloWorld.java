9cc641dfde390a724a4db4df2db9bbbf7d5df494
caoheng <caoheng@jiuhuar.com>
2019-06-22 20:26:34 +0800

    init project

---
+++ src/main/java/cn/smdq/ch/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.smdq.ch;
+
+/**
+ * @author shuimodanqing
+ * create at:  2019/6/22  8:25 PM
+ * @description
+ */
+public class HelloWorld {
+    public void print() {
+        System.out.println("Hello World!");
+    }
+}
