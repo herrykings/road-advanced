eaeaaa906fc6ec2b3fe291e68cdffa9c3e57c41e
caoheng <caoheng@jiuhuar.com>
2019-06-23 19:01:05 +0800

    update

---
+++ src/test/java/cn/smdq/ch/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package cn.smdq.ch;
+
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void createDeployment() {
+        System.out.println("LLALALLALAL");
+    }
+
+    @Test
+    public void demo1() {
+        System.out.println("BBBBBBBBBB");
+    }
+
+    @Test
+    public void demo2() {
+        System.out.println("CCCCCCCCC");
+    }
+
+}
